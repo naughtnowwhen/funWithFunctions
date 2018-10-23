e4f1fc979d80cb7105b60691dced0dfe9022d22b
Peter <naughtnowwhen@gmail.com>
2018-10-23 14:14:56 -0700

    sum and multiply pass

--- app.js
+++ app.js
@@ -30,3 +30,36 @@ var multiply = function(numA, numB){
   return multArr;
 };
   // ----------------<<<-----multiply()------------------------------------
+var sumOfFirst2;
+var sumOfThird;
+var sumOfAll;
+var myArr;
+var multOfFirst2;
+var multOfThird;
+var multOfAll;
+var stringSum;
+var stringMult;
+
+// ------------------------sumAndMultiply()------------------------------------
+
+
+sumAndMultiply = function(numA,numB,numC) {
+  myArr = [];
+  sumOfFirst2 = sum(numA,numB)[0];
+  sumOfThird = sum(numC,0)[0];
+  sumOfAll = sum(sumOfFirst2,sumOfThird)[0];
+  myArr.push(sumOfAll);
+
+  multOfFirst2 = multiply(numA,numB)[0];
+  multOfThird = multiply(numC,1)[0];
+  multOfAll = multiply(multOfFirst2,multOfThird)[0];
+  myArr.push(multOfAll);
+
+  stringSum = `${numA} and ${numB} and ${numC} sum to ${sumOfAll}.`;
+  myArr.push(stringSum);
+
+  stringMult = `The product of ${numA} and ${numB} and ${numC} is ${multOfAll}.`;
+  myArr.push(stringMult);
+
+  return myArr;
+};
